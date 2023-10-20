f041b03ad97e8448d26512af577bd787ca50f895
3gu93gvv <146980119+3gu93gvv@users.noreply.github.com>
2023-10-20 20:23:14 +0700

    Create 4j48gi.py

---
+++ 4j48gi.py
@@ -0,0 +1,89 @@
+#màu
+luc = "\033[1;32m"
+trang = "\033[1;37m"
+do = "\033[1;31m"
+vang = "\033[0;93m"
+hong = "\033[1;35m"
+xduong = "\033[1;34m"
+xnhac = "\033[1;36m"
+red='\u001b[31;1m'
+yellow='\u001b[33;1m'
+green='\u001b[32;1m'
+blue='\u001b[34;1m'
+tim='\033[1;35m'
+xanhlam='\033[1;36m'
+xam='\033[1;30m'
+black='\033[1;19m'
+import requests,os,sys
+from time import sleep
+from datetime import datetime
+day = (datetime.now().strftime('%d/%m/%Y'))
+time = (datetime.now().strftime('%H:%M:%S'))
+ip = requests.get('https://checkip.amazonaws.com').text.strip()
+info = f'{red}[{trang}+{red}]'
+hd = f'{red}[{trang}•_•{red}] {trang}=> {green}'
+gachngang = f'{red}────────────────────────────────────────────────────────────────────────'
+while True:
+ def banner():
+  os.system("cls" if os.name == "nt" else "clear")
+  banner = f'''{xduong}███╗  ██╗ █████╗ ██████╗ ██╗      ████████╗ █████╗  █████╗ ██╗
+{xduong}████╗ ██║██╔══██╗██╔══██╗██║      ╚══██╔══╝██╔══██╗██╔══██╗██║
+{xduong}██╔██╗██║██║  ██║██████╔╝██║█████╗   ██║   ██║  ██║██║  ██║██║
+{xduong}██║╚████║██║  ██║██╔══██╗██║╚════╝   ██║   ██║  ██║██║  ██║██║
+{xduong}██║ ╚███║╚█████╔╝██║  ██║██║         ██║   ╚█████╔╝╚█████╔╝███████╗
+{xduong}╚═╝  ╚══╝ ╚════╝ ╚═╝  ╚═╝╚═╝         ╚═╝    ╚════╝  ╚════╝ ╚══════╝
+{red}────────────────────────────────────────────────────────────────────────
+{info}{tim} Admin: Quang Đức
+{info}{yellow} Zalo: 0364218196
+{info}{xnhac} Box Zalo: https://zalo.me/g/ocazmg167
+{info}{blue} Youtube: https://youtube.com/@noritool
+{red}────────────────────────────────────────────────────────────────────────\n'''
+  for X in banner:
+   sys.stdout.write(X)
+   sys.stdout.flush()
+   sleep(0.00125)
+ banner()
+ def code():
+  code =f'''{do}╔═════════════════════╗
+{do}║   {vang}Code Shop Game    {do}║
+{do}╚═════════════════════╝
+{hd}Nhập Số \033[1;31m[\033[1;33m1\033[1;31m] \033[1;32mCode Bán Acc Ramdom
+{hd}Nhập Số \033[1;31m[\033[1;33m2\033[1;31m] \033[1;32mCode Bán Acc Đa Game
+{hd}Nhập Số \033[1;31m[\033[1;33m3\033[1;31m] \033[1;32mCode Bán Acc Đa Game V2
+{hd}Nhập Số \033[1;31m[\033[1;33m4\033[1;31m] \033[1;32mCode Bán Acc Đa Game V3
+{hd}Nhập Số \033[1;31m[\033[1;33m5\033[1;31m] \033[1;32mCode Bán Acc NRO
+{hd}Nhập Số \033[1;31m[\033[1;33m6\033[1;31m] \033[1;32mCode Bán Acc NRO V2
+{hd}Nhập Số \033[1;31m[\033[1;33m7\033[1;31m] \033[1;32mCode Shop Có DV Roblox
+{hd}Nhập Số \033[1;31m[\033[1;33m8\033[1;31m] \033[1;32mCode Bán Vàng Ngọc NRO
+{hd}Nhập Số \033[1;31m[\033[1;33m9\033[1;31m] \033[1;32mCode Bán Acc Free Fire
+{hd}Nhập Số \033[1;31m[\033[1;33m10\033[1;31m] \033[1;32mCode Bán Acc Liên Quân
+{hd}Nhập Số \033[1;31m[\033[1;33m11\033[1;31m] \033[1;32mCode Bán Acc Siêu Đẹp
+{hd}Nhập Số \033[1;31m[\033[1;33m12\033[1;31m] \033[1;32mCode Bán Acc LM, LQ, NRO
+{hd}Nhập Số \033[1;31m[\033[1;33m13\033[1;31m] \033[1;32mCode Code Shop Playtogether
+{hd}Nhập Số \033[1;31m[\033[1;33m14\033[1;31m] \033[1;32mCode Shop Cena FF
+{hd}Nhập Số \033[1;31m[\033[1;33m15\033[1;31m] \033[1;32mCode Shop Hải Đăng FF
+{hd}Nhập Số \033[1;31m[\033[1;33m16\033[1;31m] \033[1;32mCode Shop HeroTeam FF
+{hd}Nhập Số \033[1;31m[\033[1;33m17\033[1;31m] \033[1;32mCode Shop Bán Acc Nam CT
+{hd}Nhập Số \033[1;31m[\033[1;33m18\033[1;31m] \033[1;32mCode Shop Quý 11N
+{hd}Nhập Số \033[1;31m[\033[1;33m19\033[1;31m] \033[1;32mCode Shop MeowDgame
+{hd}Nhập Số \033[1;31m[\033[1;33m20\033[1;31m] \033[1;32mCode Shop Lão Gió
+{hd}Nhập Số \033[1;31m[\033[1;33m21\033[1;31m] \033[1;32mCode Shop Hà Mon
+{hd}Nhập Số \033[1;31m[\033[1;33m22\033[1;31m] \033[1;32mCode Shop Kelly Gaming
+{hd}Nhập Số \033[1;31m[\033[1;33m23\033[1;31m] \033[1;32mCode Shop Mr Kid
+{red}────────────────────────────────────────────────────────────────────────'''
+  print(code)
+ code()
+ chon = input(f'{hd}{luc}Nhập Lựa Chọn: {vang}')
+ if chon == '1':
+  banner()
+  print(f'{xnhac}Code Bán Acc Ramdom')
+  print(f'{hd}{luc}Link Download:')
+  print(f'{hd}{luc}Link Demo: ')
+  nhap = input(f'Bạn Muốn Tải Thêm Code Nữa Không?(Y/N):')
+  if nhap == 'n'or'N':
+   exit()
+  elif nhap == 'y'or'Y':
+   break
+  else:
+   print(f'{do}Vui Lòng Chọn Y/N Để Tiếp Tục')
+   continue
